07f6692890e87d8c0ae6d2136d58fd2aa8314c00
DorianBajorek <dorianb324@gmail.com>
2021-06-08 21:27:57 +0200

    fist Scene host...

--- src/sample/DBConnector.java
+++ src/sample/DBConnector.java
@@ -5,10 +5,13 @@ import java.sql.DriverManager;
 
 public class DBConnector {
 
-    public static String URL = "jdbc:postgresql://localhost/IdProjekt?allowMultiQueries=true";
+    //public static String URL = "jdbc:postgresql://localhost/IdProjekt?allowMultiQueries=true";
     public static String USER = "postgres";
     public static String PASSWORD = "projektid";
-
+    public static String PORT = "projektid";
+    public static String HOST = "projektid";
+    public static String DATABASE = "projektid";
+    public static String URL=null;
     public static Connection connect(){
         Connection connection = null;
         try {
--- src/sample/controllers/firstSceneController.java
+++ src/sample/controllers/firstSceneController.java
@@ -11,10 +11,20 @@ public class firstSceneController {
     public TextField loginField;
     @FXML
     public PasswordField passwordField;
+    @FXML
+    public TextField hostField;
+    @FXML
+    public TextField databaseField;
+    @FXML
+    public TextField portField;
 
     @FXML public void confirm(){
         DBConnector.USER=loginField.getText();
         DBConnector.PASSWORD=passwordField.getText();
+        DBConnector.HOST=hostField.getText();
+        DBConnector.DATABASE=databaseField.getText();
+        DBConnector.PORT=portField.getText();
+        DBConnector.URL=String.format("jdbc::postgresql://%s:%s/%s",DBConnector.HOST,DBConnector.PORT,DBConnector.DATABASE);
         Main.initializeDatabase();
         Main.initializeFunctions();
         Main.setScene("fxml/base.fxml","/sample/style/style.css");
