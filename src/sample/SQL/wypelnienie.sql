20488d1647127a75298e4417fd3ac9967f2260bc
lukasz_gro <lukaszgrobelczyk@gmail.com>
2021-06-08 19:17:24 +0200

    correct DB

--- src/sample/SQL/wypelnienie.sql
+++ src/sample/SQL/wypelnienie.sql
@@ -1,12 +1,40 @@
 INSERT INTO jezyk VALUES
-	(1, 'english'),
-	(2, 'french'),
-	(3, 'german');
+	(1, 'English'),
+	(2, 'French'),
+	(3, 'German'),
+    (4, 'Mandarin Chinese'),
+	(5, 'Turkish'),
+	(6, 'Korean'),
+    (7, 'Italian'),
+	(8, 'Polish'),
+	(9, 'Vietnamese'),
+    (10, 'Egyptian Arabic'),
+	(11, 'Japanese'),
+	(12, 'Russian'),
+    (13, 'Greek'),
+	(14, 'Hindi'),
+	(15, 'Iranian Persian');
 
 INSERT INTO wytwornia VALUES (1, 'Columbia Pictures');
 INSERT INTO wytwornia VALUES (2, '20th Century Fox');
 INSERT INTO wytwornia VALUES (3, 'Warner Bros. Pictures');
 INSERT INTO wytwornia VALUES (4, 'Universal Pictures');
+INSERT INTO wytwornia VALUES (5, 'Marvel Studios');
+INSERT INTO wytwornia VALUES (6, 'Hollywood Pictures');
+INSERT INTO wytwornia VALUES (7, 'Goldwyn Pictures');
+INSERT INTO wytwornia VALUES (8, 'Saturn Films');
+INSERT INTO wytwornia VALUES (9, 'Screen Gems');
+INSERT INTO wytwornia VALUES (10, 'DC FILMS');
+INSERT INTO wytwornia VALUES (11, 'Walt Disney Pictures');
+INSERT INTO wytwornia VALUES (12, 'Walt Disney Studios');
+INSERT INTO wytwornia VALUES (13, 'Jerry Weintraub Productions');
+INSERT INTO wytwornia VALUES (14, 'Apatow Productions');
+INSERT INTO wytwornia VALUES (15, 'The Asylum');
+INSERT INTO wytwornia VALUES (16, 'United International Pictures');
+INSERT INTO wytwornia VALUES (17, 'Paramount Pictures');
+INSERT INTO wytwornia VALUES (18, 'Miramax');
+INSERT INTO wytwornia VALUES (19, 'Lucasfilm');
+INSERT INTO wytwornia VALUES (20, 'Digital Playground');
 
 INSERT INTO gatunek VALUES
     (1, 'Action'),
@@ -16,19 +44,49 @@ INSERT INTO gatunek VALUES
     (5, 'Biography'),
     (6, 'Criminal'),
     (7, 'Comedy'),
-    (8, 'Animation');
-    
+    (8, 'Animation'),
+    (9, 'Romance'),
+    (10, 'Mystery'),
+    (11, 'Western'),
+    (12, 'Fantasy'),
+    (13, 'Horror'),
+    (14, 'Adventure'),
+    (15, 'Historical'),
+    (16, 'Satire');
+
 INSERT INTO kraj VALUES(1, 'United States');
 INSERT INTO kraj VALUES(2, 'Canada');
 INSERT INTO kraj VALUES(3, 'France');
 INSERT INTO kraj VALUES(4, 'Great Britain');
 INSERT INTO kraj VALUES(5, 'German');
 INSERT INTO kraj VALUES(6, 'New Zeland');
+INSERT INTO kraj VALUES(7, 'Poland');
+INSERT INTO kraj VALUES(8, 'Sweden');
+INSERT INTO kraj VALUES(9, 'Norway');
+INSERT INTO kraj VALUES(10, 'Australia');
+INSERT INTO kraj VALUES(11, 'Japan');
+INSERT INTO kraj VALUES(12, 'China');
+INSERT INTO kraj VALUES(13, 'Brazil');
+INSERT INTO kraj VALUES(14, 'Argentina');
+INSERT INTO kraj VALUES(15, 'South Korea');
+INSERT INTO kraj VALUES(16, 'Portugal');
+INSERT INTO kraj VALUES(17, 'RSA');
+INSERT INTO kraj VALUES(18, 'Tunis');
+INSERT INTO kraj VALUES(19, 'Egypt');
+INSERT INTO kraj VALUES(20, 'UAE');
 
 INSERT INTO pozycja VALUES(1, 'Director');
 INSERT INTO pozycja VALUES(2, 'Actor');
 INSERT INTO pozycja VALUES(3, 'Composer');
 INSERT INTO pozycja VALUES(4, 'Writer');
+INSERT INTO pozycja VALUES(5, 'Editor');
+INSERT INTO pozycja VALUES(6, 'Stage Designer');
+INSERT INTO pozycja VALUES(7, 'Producer');
+INSERT INTO pozycja VALUES(8, 'Stuntman');
+INSERT INTO pozycja VALUES(9, 'Sound Engineer');
+INSERT INTO pozycja VALUES(10, 'Colorist');
+INSERT INTO pozycja VALUES(11, 'Image Operator');
+INSERT INTO pozycja VALUES(12, 'Cameraman');
 
 INSERT INTO osoby VALUES (1, 'Quentin', 'Tarantino', 'mezczyzna', to_date('27/03/1963', 'DD/MM/YYYY'), 1);
 INSERT INTO osoby VALUES (2, 'Daniel', 'Radcliffe', 'mezczyzna', to_date('23/07/1989', 'DD/MM/YYYY'), 4);
